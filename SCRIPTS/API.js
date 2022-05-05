2cb2785d1071fd0c2d0a01444726f3f2d329a461
Marco Antonio Martinez <marcoantoniomartinezv@hotmail.com>
2022-05-05 15:42:48 -0500

    Me conecte a la BD

---
+++ SCRIPTS/API.js
@@ -0,0 +1,22 @@
+const lanzamientosr = document.getElementById("lanzamientos-recientes");
+const url="https://raw.githubusercontent.com/MarcoAntonioMV/BD_PROYECTO_WEB/master/BD_XIAOMI_SPECS.json";
+const uri="https://raw.githubusercontent.com/MarcoAntonioMV/BD_PROYECTO_WEB/master/";
+
+
+fetch(url).then(function(respuesta){
+    return respuesta.json();
+
+
+}).then(function(datos){
+    console.log(datos);
+    console.log(datos.length);
+    for(let i=0; i < datos.length; i++){
+        lanzamientosr.innerHTML+= "<a href='"+datos[i]["pagina"]+"'> <div> <img src='" + uri + datos[i]["imagen"]+"'> <h3>"+datos [i]["nombre"]+" </h3> </div> </a>";
+
+    }
+    console.log(lanzamientosr);
+    
+
+}).catch(function(error){
+    console.log(error);
+})
